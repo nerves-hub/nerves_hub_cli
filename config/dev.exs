c15881539c278c4225d68d218110b266bf6f2f3f
Justin Schneck <jschneck@mac.com>
2018-08-18 18:39:15 -0400

    Use fixtures in dev

--- config/dev.exs
+++ config/dev.exs
@@ -1,7 +1,8 @@
 use Mix.Config
 
 config :nerves_hub_cli,
-  home_dir: Path.expand(".nerves-hub")
+  home_dir: Path.expand(".nerves-hub"),
+  ca_certs: Path.expand("test/fixtures")
 
 config :nerves_hub_cli, NervesHubCLI.API,
   host: "0.0.0.0",
