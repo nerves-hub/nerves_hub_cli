6cc7f0387939088c4524a09ed75fe1d7f492188d
Frank Hunleth <fhunleth@troodon-software.com>
2019-01-30 08:55:40 -0500

    Bump nerves_hub_core

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule NervesHubCLI.MixProject do
     [
       {:pbcs, "~> 0.1"},
       {:x509, "~> 0.3"},
-      {:nerves_hub_core, "~> 0.2", github: "nerves-hub/nerves_hub_core"},
+      {:nerves_hub_core, "~> 0.3"},
       {:ex_doc, "~> 0.19", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0.0-rc.4", only: [:dev, :test], runtime: false}
     ]
