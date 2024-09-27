6e48381d0d06bc17b99859b10696a9e8b9fc4403
Gus Workman <gus.a.workman@gmail.com>
2024-09-27 10:24:01 +0200

    allow global config of product, org

--- lib/nerves_hub_cli/cli/config.ex
+++ lib/nerves_hub_cli/cli/config.ex
@@ -2,19 +2,20 @@ defmodule NervesHubCLI.CLI.Config do
   alias NervesHubCLI.CLI.Shell
 
   @switches []
-  @valid_config_keys ["uri"]
+  @valid_config_keys ["uri", "product", "org"]
 
   def run(args) do
     {_opts, args} = OptionParser.parse!(args, strict: @switches)
 
     case args do
       ["set", key, value] when key in @valid_config_keys ->
-        String.to_existing_atom(key)
+        # TODO: double check the usage of `String.to_atom/1` here. Should be safe since guarded
+        String.to_atom(key)
         |> NervesHubCLI.Config.put(value)
 
       ["get", key] when key in @valid_config_keys ->
         value =
-          String.to_existing_atom(key)
+          String.to_atom(key)
           |> NervesHubCLI.Config.get()
 
         Shell.info("#{key}: #{value}")
