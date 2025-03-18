2f1c5a8021ef9d9b65f2c60b19a5aabcd1bee1f6
Josh Kalderimis <josh.kalderimis@gmail.com>
2025-03-18 20:26:47 +1300

    Correctly handle validation errors from hub
    Also make sure a newline is added

--- lib/nerves_hub_cli/cli/shell.ex
+++ lib/nerves_hub_cli/cli/shell.ex
@@ -8,7 +8,7 @@ defmodule NervesHubCLI.CLI.Shell do
 
   @spec error(IO.ANSI.ansidata()) :: :ok
   def error(message) do
-    IO.puts(:stderr, IO.ANSI.format(red(message)))
+    IO.puts(:stderr, IO.ANSI.format(red("\n#{message}")))
   end
 
   defp red(message) do
@@ -122,6 +122,11 @@ defmodule NervesHubCLI.CLI.Shell do
     error("HTTP error")
     for {key, reason} <- reasons, do: error("  #{key}: #{reason}")
     :ok
+  end
+
+  def do_render_error({:error, %{"errors" => %{"detail" => reason}}}) when is_binary(reason) do
+    error("HTTP error")
+    error(reason)
   end
 
   def do_render_error(error) do
